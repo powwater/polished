83082c0ed1dd1a74c225fc5a1e36a22541ac97e9
Patrick Howard <patrick.howard@tychobra.com>
2021-03-25 12:34:14 -0400

    User edit bug fix

--- R/user_edit_module.R
+++ R/user_edit_module.R
@@ -98,13 +98,10 @@ user_edit_module <- function(input, output, session,
       }
 
       email_input <- NULL
+      phone_input <- NULL
 
       send_invite_ui <- list()
     }
-
-
-
-
 
     shiny::showModal(
       shiny::modalDialog(
@@ -189,8 +186,9 @@ user_edit_module <- function(input, output, session,
     session_user <- session$userData$user()$user_uid
     # TODO:
     #   FOR PLUMBER, send `NULL` to use default `NA` in endpoints
-    input_email <- if (input$user_email == "") NULL else tolower(input$user_email)
-    input_phone <- input$user_phone
+
+    input_email <- if (length(input$user_email) == 0) NULL else tolower(input$user_email)
+    input_phone <- if (length(input$user_phone) == 0) NULL else input$user_phone
     input_is_admin <- input$user_is_admin
 
     is_admin_out <- if (input_is_admin == "Yes") TRUE else FALSE
